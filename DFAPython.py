9ce449e3e9949efff5a38667369d9b3f29076247
Ioan George Istrate <istrate_ioan_george@yahoo.com>
2015-03-02 18:30:10 -0500

    input file, user input loops

--- DFAPython.py
+++ DFAPython.py
@@ -1,7 +1,23 @@
-
-
+#
+#   Python DFA
+#
 def main(): 
-    print("Hello")
+    
+    running = True;
+    while(running): 
+        #open file
+        uInput = input("Please enter filename:" )
+        try:
+            file = open(uInput)
+            running = False
+        except:
+            print("Invalid file")
+    
+    running = True;
+    while(running):
+        uInput = input("Please enter string: ")
+        if (uInput.lower == "quit"):
+            running = False
 
 
 if __name__ == '__main__':
