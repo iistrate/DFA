6e9a5af19e07e780459511d20805d43d5cf319fe
Ioan George Istrate <istrate_ioan_george@yahoo.com>
2015-03-02 19:47:56 -0500

    Node class, test cases, load file

--- DFAPython.py
+++ DFAPython.py
@@ -1,24 +1,70 @@
 #
 #   Python DFA
 #
+import Node
+
 def main(): 
-    
+    #get filename to open    
     running = True;
     while(running): 
-        #open file
-        uInput = input("Please enter filename:" )
+        #uInput = input("Please enter filename:" )
+        #remove
+        uInput = "t.txt"
+        #remove
         try:
             file = open(uInput)
             running = False
         except:
             print("Invalid file")
-    
-    running = True;
-    while(running):
-        uInput = input("Please enter string: ")
-        if (uInput.lower == "quit"):
-            running = False
 
+    #parse file and store states
+    counter = 0
+    transitions = []
+    finalStates = []
+    for line in file:
+        #first line contains final states
+        if (counter == 0):
+            #store final states
+            finalStates = line.split(' ')
+        #store transitions
+        else:
+            line = line.split(' ')
+            transitions.append(Node.Node(line[0], line[1], line[2]))
+        counter += 1
+
+    #done with file so close
+    file.close
+
+    #get string to check against
+    #running = True;
+    #while(running):
+    #    uInput = input("Please enter string: ")
+    #    if (uInput.lower == "quit"):
+    #        running = False
+
+    testCase = "aaaaaabbbbbababbbbbb";
+
+    #we always begin at 0
+    pointer = 0
+    error = False
+    counter = 0
+    for letter in testCase:
+        #check if letter is in transition at pointed index
+        if (letter == transitions[pointer].mLetter):
+            #if letter is last letter and it is in a final state then string is accepted
+            if ((pointer in finalStates) and counter == len(testCase)):
+                break
+            #if letter in node then advance to next node
+            pointer = transitions[pointer].mFinal
+        #if letter not in language set error flag and quit
+        else:
+            error = True
+            break
+        counter += 1
+    if (error):
+        print("Not valid!")
+    else:
+        print("String accepted")
 
 if __name__ == '__main__':
     main()
---
+++ Node.py
@@ -0,0 +1,9 @@
+class Node(object):
+    """Node"""
+    def __init__(self, initial, keys, final):
+        self.mInitial = int(initial)
+        self.mLetter = keys
+        self.mFinal = int(final)
+
+
+
